e191c0109b0bb36e445cc29ad056e5894f2dda02
jinwoo2361 <jinwoo236161@gmail.com>
2024-01-08 20:18:59 +0900

    no message

--- crud_hotel/src/main/java/com/sh/crud/booking/controller/BookingCheckServlet.java
+++ crud_hotel/src/main/java/com/sh/crud/booking/controller/BookingCheckServlet.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 @WebServlet("/booking/bookingCheck")
 public class BookingCheckServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
         req.getRequestDispatcher("/WEB-INF/views/booking/bookingCheck.jsp").forward(req, resp);
     }
 }
